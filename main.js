3c372f1cec72ce560f8035db72d07f044caad3cc
Edward Ding <nsobject.ding.jiarui@gmail.com>
2024-02-04 23:51:12 -0800

    Remove individual links and update stl zip

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-// Sample list of objects with data
-var dataList = [
-    { index: 1, staticImage: 'public/images/1.PNG', downloadLink: 'https://2ly.link/1vw2e' },
-    { index: 2, staticImage: 'public/images/2.PNG', downloadLink: 'https://2ly.link/1vw2e' },
-    // Add more objects as needed
-];
+var dataList = [];
+for (var idx = 1; idx <= 30; idx++) {
+    var item = {
+        index: idx,
+        staticImage: `public/images/${idx}.PNG`,
+        threeDImage: `public/gifs/${idx}.gif`,
+        // downloadLink: `https://2ly.link/1vw2e`,
+    };
+
+    dataList.push(item);
+}
 
 // Function to dynamically create a table with data
 function createTable() {
     // Create a table element
     var table = document.createElement('table');
 
-    // Create table header
+    // Create table header  
     var headerRow = table.insertRow();
     headerRow.insertCell(0).innerHTML = 'Object Index';
     headerRow.insertCell(1).innerHTML = 'Static Image';
-    // headerRow.insertCell(2).innerHTML = '3D Image';
+    headerRow.insertCell(2).innerHTML = '3D Image';
     // headerRow.insertCell(3).innerHTML = 'Download Link';
-    headerRow.insertCell(2).innerHTML = 'STL File Download Link';
+    // headerRow.insertCell(2).innerHTML = 'STL File Download Link';
 
     // Iterate through the data list and populate the table
     for (var i = 0; i < dataList.length; i++) {
@@ -27,10 +32,10 @@ function createTable() {
 
         // Insert cells and fill them with data
         row.insertCell(0).innerHTML = data.index;
-        row.insertCell(1).innerHTML = '<img src="' + data.staticImage + '" alt="Static Image">';
-        // row.insertCell(2).innerHTML = '<img src="' + data.threeDImage + '" alt="3D Image">';
+        row.insertCell(1).innerHTML = '<img class="column-2-image" src="' + data.staticImage + '" alt="Static Image" loading="lazy">';
+        row.insertCell(2).innerHTML = '<img class="column-3-image" src="' + data.threeDImage + '" alt="3D Image" loading="lazy">';
         // row.insertCell(3).innerHTML = '<a href="' + data.downloadLink + '" download>Download</a>';
-        row.insertCell(2).innerHTML = '<a href="' + data.downloadLink + '" download>Download</a>';
+        // row.insertCell(2).innerHTML = '<a href="' + data.downloadLink + '" download>Download</a>';
     }
 
     // Append the table to the body or any other HTML element
